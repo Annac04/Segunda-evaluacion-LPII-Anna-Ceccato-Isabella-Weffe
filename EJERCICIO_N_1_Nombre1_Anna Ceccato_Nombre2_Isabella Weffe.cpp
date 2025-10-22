79400141c815d16387a7c40c62a72163724faa9a
Anna <annaccallamo@gmail.com>
2025-10-22 16:46:06 -0400

    Add files via upload

---
+++ EJERCICIO_N_1_Nombre1_Anna Ceccato_Nombre2_Isabella Weffe.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+int main () {
+	char numerobinario=0, i=0;
+	int validacion;
+	int decimal=0;
+	float base;
+	char key;
+	int c= 0;
+
+	do{
+		cout<<"ingrese el numero binario(solo digitos 0 y 1):"<< endl;
+	float numerobinario;
+	  
+	for (char numerobinario) {
+		if ( c!= "0" && c!= "1")
+		cout<< "solo se permite numeros que contenga 0 y 1:" << endl ;
+		return 1;
+	}
+		
+			{
+				numerobinario*base(10)
+				for ( int i=0; i < base;++i){
+					numerobinario[i]=="1")
+					decimal+= pow (2; base; -1-i);
+					
+				}
+			}
+			cout <<"el numero decimal es"<<decimal <<endl;
+			
+			cout << "si desea continuar ingrese la letra 'c', sino ingrese cualquier otra"<< endl;
+		}while( key == 'c'|| key == 'C');
+		
+		return 0;
+	
+	
+	}
+	
